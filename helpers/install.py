1e716857c9a952f2378d8dba44f11a1a818c9cc2
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2023-01-19 07:48:51 +0100

    auto: update install.py

--- helpers/install.py
+++ helpers/install.py
@@ -3,11 +3,6 @@ import sys
 
 
 def install_requirements():
-    """
-    Install the requirements.txt file.
-
-    This is a helper function to install the requirements.txt file
-    in the current environment.
-    """
+    """Install the requirements.txt file."""
     subprocess.check_call([sys.executable, "-m", "pip",
                           "install", "-r", "requirements.txt"])
