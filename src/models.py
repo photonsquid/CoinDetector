83dd764eb6a7e0ca8953f9ddbea5a15b98fb1094
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 02:06:44 +0100

    auto: update models.py

--- src/models.py
+++ src/models.py
@@ -1,10 +1,33 @@
-from keras.layers import Dense, Input
+from keras.layers import Conv2D, Dense, Flatten, Input, MaxPooling2D
 from keras.models import Model
 
 from src.l1_dist import L1Dist
 
 
-def make_siamese_model():
+def make_embedding():
+    inp = Input(shape=(105, 105, 3), name='input_image')
+
+    # First block
+    c1 = Conv2D(64, (10, 10), activation='relu')(inp)
+    m1 = MaxPooling2D(64, (2, 2), padding='same')(c1)
+
+    # Second block
+    c2 = Conv2D(128, (7, 7), activation='relu')(m1)
+    m2 = MaxPooling2D(64, (2, 2), padding='same')(c2)
+
+    # Third block
+    c3 = Conv2D(128, (4, 4), activation='relu')(m2)
+    m3 = MaxPooling2D(64, (2, 2), padding='same')(c3)
+
+    # Final embedding block
+    c4 = Conv2D(256, (4, 4), activation='relu')(m3)
+    f1 = Flatten()(c4)
+    d1 = Dense(4096, activation='sigmoid')(f1)
+
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
+
+
+def make_siamese_model(embedding):
 
     # Anchor image input in the network
     input_image = Input(name='input_img', shape=(105, 105, 3))
