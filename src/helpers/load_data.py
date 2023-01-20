1d463c965ba80864fca3305ca0c9733c13e49501
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 13:05:58 +0100

    auto: update load_data.py

---
+++ src/helpers/load_data.py
@@ -0,0 +1,77 @@
+
+import os
+
+import cv2
+
+
+def load_data(path: str):
+    # load the data from the path
+    # this will return a dictionary with the following structure
+    # {
+    #   "test": [
+    #       {
+    #           "image": <image>,
+    #           "label": {
+    #               "country": <string>,
+    #               "value": <string>,
+    #               "specificity": <string>,
+    #               "id": <string>
+    #           }
+    #       },
+    #       ...
+    #   ],
+    #   "train": [
+    #       {
+    #           "image": <image>,
+    #           "label": {
+    #               "country": <string>,
+    #               "value": <string>,
+    #               "specificity": <string>,
+    #               "id": <string>
+    #           }
+    #       },
+    #       ...
+    #   ]
+    # }
+
+    # get all folder names in the path
+    folders = os.listdir(path)
+
+    dataset = {}
+
+    for folder in folders:
+
+        dataset[folder] = []
+
+        # get all file names in the folder
+        files = os.listdir(os.path.join(path, folder))
+
+        for file in files:
+            # compute from the file name the country, value, specificity and id
+            # pattern: country_value_specificity_id.png or country_value_id.png (specificity is optional)
+            # example: italy_1_cent_2000_1.png or italy_1_cent_1.png
+            file_name = file.split(".")[0]
+            file_name = file_name.split("_")
+            country = file_name[0]
+            value = file_name[1]
+            id = file_name[-1]
+            if len(file_name) == 5:
+                specificity = file_name[2]
+            else:
+                specificity = ""
+
+            # load the image from the file
+            image = cv2.imread(os.path.join(path, folder, file))
+
+            # add the image to the dataset
+            dataset[folder].append({
+                "image": image,
+                "label": {
+                    "country": country,
+                    "value": value,
+                    "specificity": specificity,
+                    "id": id
+                }
+            })
+
+    pass
