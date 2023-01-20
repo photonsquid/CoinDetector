93e7129e7a281b27833ebf0ecc429f3dfcaaa635
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 02:03:05 +0100

    auto: update L1Dist.py

---
+++ src/L1Dist.py
@@ -0,0 +1,9 @@
+class L1Dist(Layer):
+
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
