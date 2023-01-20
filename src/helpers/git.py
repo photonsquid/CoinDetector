e1fcf54879d00714bfe7a05647e507f8be2ccec1
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 02:24:21 +0100

    auto: update git.py

---
+++ src/helpers/git.py
@@ -0,0 +1,22 @@
+# execute shell commands
+
+import subprocess
+
+
+def execute(command, show_output=False):
+    """Execute a shell command and return its output"""
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if show_output:
+        print(output)
+    return output
+
+
+def git_update(branch: str = "main", force: bool = False):
+    """Update the git repository"""
+    execute('git fetch --all')
+    execute('git checkout {}'.format(branch))
+    if force:
+        execute('git reset --hard')
+    execute('git pull')
