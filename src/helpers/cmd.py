4d9d768d9e36002956b225ce3ec50b65cc37a279
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 02:37:26 +0100

    auto: update cmd.py

--- src/helpers/cmd.py
+++ src/helpers/cmd.py
@@ -8,6 +8,11 @@ def execute(command, show_output=False):
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
+    if error:
+        error = error.decode('utf-8')
+        print(error)
+        raise Exception(error)
+    output = output.decode('utf-8')
     if show_output:
         print(output)
     return output
