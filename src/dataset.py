ad3e0e3fca33ee29e315acc03075435d4ae5f581
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 11:46:08 +0100

    auto: update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -14,7 +14,7 @@ def preprocess(file_path):
     # Preprocessing steps - resizing the image to be 100x100x3
     img = tf.image.resize(img, (100, 100))
     # Scale image to be between 0 and 1
-    img = img / 255.0
+    img = img / 255.0  # type: ignore
 
     # Return image
     return img
