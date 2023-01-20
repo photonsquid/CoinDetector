206b9b647f1c63808d12eb03a153f54e213f7dd0
Sébastien PONT <seba12.04@orange.fr>
2023-01-20 02:40:16 +0100

    auto: update setup.py

---
+++ src/helpers/setup.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
